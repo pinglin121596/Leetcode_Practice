8118d042b058c03e8188ac8047d9c3e178f28957
pinglin121596 <54443732+pinglin121596@users.noreply.github.com>
2020-05-02 21:23:27 -0400

    Update TwoSum_mySol.java
    Thinking Process: I just need to add up two numbers to the target, so I want to keep one value fixed and try to find the value that match this current one such that they can add up to the target. Hence, one inner loop with one outer loop  are what come to my mind first(aware that the speed will be slow ). At first, I failed to consider the constrain that one element can only be used once.

--- TwoSum_mySol.java
+++ TwoSum_mySol.java
@@ -4,8 +4,8 @@ class Solution {
         result[0] = 0;
         result[1] = 1;
             
-        for(int i = 0; i <= nums.length - 2; i++){
-            for(int j = i + 1; j <= nums.length - 1; j++){
+        for(int i = 0; i <= nums.length - 2; i++){ \\ fix one value
+            for(int j = i + 1; j <= nums.length - 1; j++){ \\ go through the whole array to find the other value
                 if(nums[i] + nums[j] == target){
                     result[0] = i;
                     result[1] = j;           
